44418ad230712e45c79683dc571ab66119e91514
Brandon Fulljames <bfulljames@blizzard.com>
2017-02-28 19:07:57 -0800

    Some simple spell details

--- src/components/SpellDetails.jsx
+++ src/components/SpellDetails.jsx
@@ -8,8 +8,17 @@ module.exports = React.createClass({
     spellData: React.PropTypes.object.isRequired
   },
   render: function() {
+    const s = this.props.spellData;
+    const classes = s.Classes.join(', ');
     return (
-      <h1>{this.props.spellData.Name}</h1>
+      <div className="spell-details">
+        <h1>{s.Name}</h1>
+        <h3>{s.Level} {s.School} ({classes})</h3>
+        <p>{s.CastTime} - {s.Components}</p>
+        <p>{s.Duration}</p>
+        <p>{s.Description}</p>
+        <p>{s.SourceBook} - {s.SourcePage}</p>
+      </div>
     );
   }
 });
--- src/stores/spellStore.js
+++ src/stores/spellStore.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const EventEmitter = require('events').EventEmitter;
+
+const spells = [];
+
+class SpellStore extends EventEmitter {
+  getAll() {
+    return spells;
+  }
+}
+
+module.exports = new SpellStore();
