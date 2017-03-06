d6687ea5e61af8900a2465150b778699826866eb
Brandon Fulljames <bfullj@gmail.com>
2017-03-05 23:47:48 -0800

    Remove errant console log

--- src/containers/ClassSpells.jsx
+++ src/containers/ClassSpells.jsx
@@ -4,7 +4,6 @@ const SpellList = require('../components/SpellList');
 const connect = require('react-redux').connect;
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
   return {
     spells: state.spells.byClass[ownProps.params.selectedClass.toLowerCase()].map(n => state.spells.all[n])
   };
